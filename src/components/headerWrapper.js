d237802ca85dcd283ac55cd30957629ea6137a78
Miguel Rust <hava12soft@gmail.com>
2018-07-10 09:54:14 -0600

    Built out initial styles>

--- src/components/headerWrapper.js
+++ src/components/headerWrapper.js
@@ -4,8 +4,8 @@ class HeaderWrapper extends Component{
   render(){
     return(
       <div className="header-wrapper">
-        <h1>HOA Manager</h1>
-        <p>A property managment solution.</p>
+        <h1 className="header-wrapper__title">HOA Manager</h1>
+        <p className="header-wrapper__subtitle">A property managment solution.</p>
         {this.props.children}
       </div>
     );
