6eeb2c5d4dcef96a404b32a30b7fe0809ea8c478
Miguel Rust <hava12soft@gmail.com>
2018-07-18 08:25:52 -0600

    Added support for real dates in archive items component.

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -4,7 +4,7 @@ function ArchiveItem({title, date}){
   return(
     <div className="archive-item archive-items__item">
       <div className="archive-item__title">{title}</div>
-      <div className="archive-item__date">{date}</div>
+      <div className="archive-item__date">{date.getUTCMonth()+1}/{date.getDate()}/{date.getFullYear()-2000}</div>
     </div>
   );
 }
@@ -16,9 +16,9 @@ class NewsletterArchive extends Component{
       <div className="newsletter-archive">
         <div className="newsletter-archive__title">{title}</div>
         <div className="newsletter-archive__items .archive-items">
-          <ArchiveItem title="Hello World" date="1/1/2001" />
-          <ArchiveItem title="Hello World" date="1/1/2001" />
-          <ArchiveItem title="Hello World" date="1/1/2001" />
+          <ArchiveItem title="Hello World" date={new Date()} />
+          <ArchiveItem title="Hello World" date={new Date()} />
+          <ArchiveItem title="Hello World" date={new Date()} />
         </div>
       </div>
     );
