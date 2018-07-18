29b45a9b4512ef55737ce0485457597e1035e5e3
Miguel Rust <hava12soft@gmail.com>
2018-07-18 11:12:16 -0600

    Continued refactoring form styles.

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -6,14 +6,8 @@ class NewsletterNewForm extends Component{
   render(){
     const {handleSubmit} = this.props;
     return(
-      <form className="sign-in-form" onSubmit={handleSubmit}>
-        <Field component={FormInput} name="email" title="Email" type="email" className="sign-in-form__email" placeholder="Enter Email" />
-        <Field component={FormInput} name="password" title="Password" type="password" className="sign-in-form__email" placeholder="Enter Password" />
-        <Field name="login" title="Log In" className="sign-in-form__log-in" component={FormButton} type="submit"/>
-        <div className="sign-in-form__text-links">
-          <TextLink text="Forgot Password" to="/forgotpassword" />
-          <TextLink text="Not a member? Register here." to="/signup" />
-        </div>
+      <form className="new-newsletter-form" onSubmit={handleSubmit}>
+        <Field component={FormInput} name="email" title="Email" type="email" className="new-newsletter-form__email" placeholder="Enter Email" />
       </form>
     );
   }
