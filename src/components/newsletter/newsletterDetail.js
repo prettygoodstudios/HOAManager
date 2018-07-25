ead088d59edbe166f479ccbc59fd24e5d87a9a5c
Miguel Rust <hava12soft@gmail.com>
2018-07-25 09:32:51 -0600

    Built out initial newsletter detail page.

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -1,6 +1,15 @@
 import React, {Component} from "react";
 
+import {connect} from "react-redux";
+
+import * as actions from "../../actions";
+
 class NewsletterDetail extends Component {
+
+
+  componentDidMount() {
+    this.props.fetchNewsletterWithId(this.props.match.params.id);
+  }
   render(){
     return(
       <div className="newsletter-detail">
@@ -10,4 +19,11 @@ class NewsletterDetail extends Component {
   }
 }
 
-export default NewsletterDetail;
+function mapStateToProps(state){
+  const {newsletterToEdit} = state.newsletter;
+  return {
+    newsletterToEdit
+  }
+}
+
+export default connect(mapStateToProps, actions)(NewsletterDetail);
