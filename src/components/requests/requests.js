0e727361805de680d8f215ef91f93cc0305ffac8
Miguel Rust <hava12soft@gmail.com>
2018-07-30 08:16:43 -0600

    Built requests grid.

---
+++ src/components/requests/requests.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Requests extends Component{
+  render(){
+    return(
+      <div className='requests'>
+
+      </div>
+    )
+  }
+}
+export default Requests;
---
+++ src/components/requests/requestsBoxes.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class RequestsBoxes extends Component{
+  render(){
+    return(
+      <div className='requests-boxes'>
+
+      </div>
+    );
+  }
+}
+export default RequestsBoxes;
