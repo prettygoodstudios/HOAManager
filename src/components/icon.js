537cc646467f99eef4e272dc2bad26b147f0f810
Miguel Rust <hava12soft@gmail.com>
2018-07-30 09:47:04 -0600

    Built out JSX scaffold for requests item.

---
+++ src/components/icon.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function icon(i, className){
+  return(
+    <div className={className}>
+      <i className={i}></i>
+    </div>
+  );
+}
--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,9 +1,23 @@
 import React, {Component} from "react";
+import icon from "../icon";
+import Button from '../button';
 
 class RequestsItem extends Component{
   render(){
     return(
       <div className="requests-item">
+        {icon("fas fa-exclamation-triangle","requests-item__icon")}
+        <div className="requests-item__title">
+          The power is out.
+        </div>
+        <div className="requests-item__tenant-unit">
+          Joe - 223
+        </div>
+        {icon("fas fa-sort-down","requests-item__arrow")}
+        <div className="requests-item__date">
+          2/2/2016
+        </div>
+        <Button icon="fas fa-wrench" className="requests-item__move" callback={() => console.log("Change Request status.")}/>
       </div>
     );
   }
